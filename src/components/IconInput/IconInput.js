3557205679922810fb864d65a6f32a112476a4f0
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-02-28 17:05:24 +0100

    start iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {},
+  large: {},
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <Wrapper style={{ width, ...STYLES[size] }}>
+      <Icon id={icon} />
+      <NativeInput type="text" placeholder={placeholder} aria-label={label} />
+    </Wrapper>
+  );
+};
+
+const NativeInput = styled.input`
+  position: absolute;
+  inset: ;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
 
 export default IconInput;
