2145d92ce4514811495d5114b9cbf10866d325bb
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-02-28 16:31:11 +0100

    attempt at select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+// import { getDisplayedValue } from "./Select.helpers";
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  display: inline-block;
+  position: relative;
+  color: ${COLORS.gray700};
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.1875;
+`;
+
+const NativeSelect = styled.select`
+  cursor: pointer;
+  display: inline-block;
+  appearance: none;
+  background: transparent;
+  padding: 0.75rem 3.25rem 0.75rem 1rem;
+  border: none;
+  color: inherit;
+  font-size: inherit;
+  line-height: inherit;
+`;
+
+const DropDownIcon = styled(Icon)`
+  position: absolute;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  // const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <Wrapper>
+        <NativeSelect value={value} onChange={onChange} aria-label={label}>
+          {children}
+        </NativeSelect>
+        <DropDownIcon id="chevron-down" size="24" strokeWidth={2} />
+      </Wrapper>
+    </>
   );
 };
 
